51cf29cbcecea74de97ce362336f9853bc9c45a6
eakinloose <eakinloose@gmail.com>
2022-10-30 11:20:25 +0100

    events

--- src/Pages/Events/Events.js
+++ src/Pages/Events/Events.js
@@ -3,41 +3,42 @@ import React from "react";
 import { catevents } from "./eventsdata";
 import { EventsWrapper } from "./EventsStyles";
 
-setInterval(function updateTimer() {
-   let future = Date.parse("October 29, 2022 12:00:00");
-   let now = new Date();
-   let diff = future - now;
+// setInterval(function updateTimer() {
+//    let future = Date.parse("October 29, 2022 12:00:00");
+//    let now = new Date();
+//    let diff = future - now;
 
-   let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-   let hours = Math.floor(diff / (1000 * 60 * 60));
-   let mins = Math.floor(diff / (1000 * 60));
-   let secs = Math.floor(diff / 1000);
+//    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+//    let hours = Math.floor(diff / (1000 * 60 * 60));
+//    let mins = Math.floor(diff / (1000 * 60));
+//    let secs = Math.floor(diff / 1000);
 
-   let d = days;
-   let h = hours - days * 24;
-   let m = mins - hours * 60;
-   let s = secs - mins * 60;
+//    let d = days;
+//    let h = hours - days * 24;
+//    let m = mins - hours * 60;
+//    let s = secs - mins * 60;
 
-   document.getElementById("timer").innerHTML =
-      "<div>" +
-      d +
-      "<span>days</span></div>" +
-      "<div>" +
-      h +
-      "<span>hours</span></div>" +
-      "<div>" +
-      m +
-      "<span>minutes</span></div>" +
-      "<div>" +
-      s +
-      "<span>seconds</span></div>";
-}, 1000);
+//    document.getElementById("timer").innerHTML =
+//       "<div>" +
+//       d +
+//       "<span>days</span></div>" +
+//       "<div>" +
+//       h +
+//       "<span>hours</span></div>" +
+//       "<div>" +
+//       m +
+//       "<span>minutes</span></div>" +
+//       "<div>" +
+//       s +
+//       "<span>seconds</span></div>";
+// }, 1000);
 
 const Events = () => {
    return (
       <EventsWrapper>
-         <div id="timer"></div>
-         <h3 className="info-">TO OUR SPECIAL TEAM BONDING EXERCISE!</h3>
+         {/* <div id="timer"></div> */}
+         <h2 className="info-">OUR TEAM BONDING EXERCISE WAS FUN!</h2>
+         <h3>29th OCT 2022</h3>
          <br />
          <hr />
          {catevents.map((item, index) => {
