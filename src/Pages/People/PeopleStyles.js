31341f94d8d61c1a58354e2a2701a3a2df6a433e
eakinloose <eakinloose@gmail.com>
2022-12-26 09:01:40 +0100

    fixes

--- src/Pages/People/PeopleStyles.js
+++ src/Pages/People/PeopleStyles.js
@@ -5,27 +5,27 @@ export const PeopleWrapper = styled.div`
    max-width: 1470px;
    margin: 6rem auto;
    padding: 4rem 0 0 0;
-   
-   .container{
+
+   .container {
       .heading-title {
          margin-bottom: 3rem;
          text-align: center;
          margin-bottom: 0;
          letter-spacing: 2px;
-   
+
          h3 {
             font-weight: 400;
             text-transform: uppercase;
          }
       }
-   
+
       .flex {
          margin: auto;
          display: grid;
          grid-template-columns: repeat(3, 1fr);
          width: 100%;
          gap: 3rem;
-   
+
          .col-md-4 {
             width: 100%;
             .team-member {
@@ -44,24 +44,22 @@ export const PeopleWrapper = styled.div`
          p {
             margin: -10px 0;
          }
-   }
 
+         @media screen and (max-width: 999px) {
+            grid-template-columns: repeat(2, 1fr);
+            width: 100%;
+         }
 
-      @media screen and (max-width: 999px) {
-         grid-template-columns: repeat(2, 1fr);
-         width: 100%;
+         @media screen and (max-width: 599px) {
+            grid-template-columns: repeat(1, 1fr);
+            padding: 3rem 0;
+            width: 100%;
+         }
       }
-
-      @media screen and (max-width: 599px) {
-         grid-template-columns: repeat(1, 1fr);
-         padding: 3rem 0;
-         width: 100%;
-      }
    }
-
 
    @media screen and (max-width: 599px) {
-         padding: 0rem 0;
-         width: 98%;
-      }
+      padding: 0rem 0;
+      width: 98%;
+   }
 `;
