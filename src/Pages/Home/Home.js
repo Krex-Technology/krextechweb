e7f7e44d38fdefb386c8aff102cf00601ede90a3
Samuel Osinubi <samobafemisayo@gmail.com>
2023-09-08 14:59:08 +0100

    update2

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,7 +2,7 @@ import { HomeWrapper } from "./HomeStyles";
 import dcb from "../../assets/images/dcb11.png";
 import About from "../../Components/About/About";
 import Offer from "../../Components/Offer/Offer";
-import BlogSlider from "../../Components/BlogSlider/BlogSlider";
+// import BlogSlider from "../../Components/BlogSlider/BlogSlider";
 
 
 const Home = () => {
